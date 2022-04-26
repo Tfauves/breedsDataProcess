bbe3873954ecdbdc7fa7277970aa12126f9f1792
Tranquility Base <tfauvelle@gmail.com>
2022-04-25 20:47:11 -0400

    read to repository

--- src/main/java/com/batch/springBatch/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.batch.springBatch.repositories;
-
-import com.batch.springBatch.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
